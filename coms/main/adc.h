9464ef1be3268f8abc689309230c228a55a63dc8
Connor Behrend <cbehrend@purdue.edu>
2025-10-12 18:40:25 -0400

    Use timer to sample at 100Hz per adc channel

---
+++ coms/main/adc.h
@@ -0,0 +1,11 @@
+/*
+ * ============================================================================
+ * Filename   : adc.h
+ * Author     : Connor Behrend
+ * Description: Main app task for coms subsystem
+ * ============================================================================
+ */
+
+void init_adc();
+void adc_collection_start();
+void adc_collection_stop();
