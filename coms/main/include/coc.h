2d5a26be1449b6451449c3dbee10f22fcaa41dba
Connor Behrend <cbehrend@purdue.edu>
2025-11-23 05:27:12 -0500

    Fix coc payload loss from no mbuf overhead

--- coms/main/include/coc.h
+++ coms/main/include/coc.h
@@ -7,6 +7,14 @@
 #define COC_BUF_COUNT (20 * MYNEWT_VAL(BLE_L2CAP_COC_MAX_NUM))
 #define MTU 512
 
+// https://mynewt.apache.org/latest/os/core_os/mbuf/mbuf.html
+
+#define COC_MBUF_PAYLOAD_SIZE (MTU)
+#define COC_MBUF_MAGICAL_NUMBER (24)
+#define COC_MBUF_MEMBLOCK_SIZE (COC_MBUF_PAYLOAD_SIZE + COC_MBUF_MAGICAL_NUMBER)
+#define COC_MBUF_MEMPOOL_SIZE                                                  \
+  OS_MEMPOOL_SIZE(COC_BUF_COUNT, COC_MBUF_MEMBLOCK_SIZE)
+
 int bleprph_l2cap_coc_event_cb(struct ble_l2cap_event *event, void *arg);
 void bleprph_l2cap_coc_mem_init(void);
 
--- coms/main/src/coc.c
+++ coms/main/src/coc.c
@@ -9,8 +9,7 @@
 #include "nimble/nimble_port_freertos.h"
 #include "services/gap/ble_svc_gap.h"
 
-uint16_t psm = 0x1002;
-static os_membuf_t sdu_coc_mem[OS_MEMPOOL_SIZE(COC_BUF_COUNT, MTU)];
+static os_membuf_t sdu_coc_mem[COC_MBUF_MEMPOOL_SIZE];
 static struct os_mempool sdu_coc_mbuf_mempool;
 static struct os_mbuf_pool sdu_os_mbuf_pool;
 static uint16_t peer_sdu_size;
@@ -78,7 +77,8 @@ int bleprph_l2cap_coc_event_cb(struct ble_l2cap_event *event, void *arg) {
 
   case BLE_L2CAP_EVENT_COC_DATA_RECEIVED:
     if (event->receive.sdu_rx != NULL) {
-      MODLOG_DFLT(INFO, "Data received : ");
+      MODLOG_DFLT(INFO,
+                  "Data received (%d bytes): ", event->receive.sdu_rx->om_len);
       for (int i = 0; i < event->receive.sdu_rx->om_len; i++) {
         ESP_LOGI(TAG, "%d ", event->receive.sdu_rx->om_data[i]);
       }
@@ -93,6 +93,7 @@ int bleprph_l2cap_coc_event_cb(struct ble_l2cap_event *event, void *arg) {
     peer_sdu_size = event->accept.peer_sdu_size;
     bleprph_l2cap_coc_accept(event->accept.conn_handle,
                              event->accept.peer_sdu_size, event->accept.chan);
+    MODLOG_DFLT(INFO, "Peer SDU Size = %d bytes", peer_sdu_size);
     return 0;
 
   default:
@@ -101,10 +102,10 @@ int bleprph_l2cap_coc_event_cb(struct ble_l2cap_event *event, void *arg) {
 }
 void bleprph_l2cap_coc_mem_init(void) {
   int rc;
-  rc = os_mempool_init(&sdu_coc_mbuf_mempool, COC_BUF_COUNT, MTU, sdu_coc_mem,
-                       "coc_sdu_pool");
+  rc = os_mempool_init(&sdu_coc_mbuf_mempool, COC_BUF_COUNT,
+                       COC_MBUF_MEMBLOCK_SIZE, sdu_coc_mem, "coc_sdu_pool");
   assert(rc == 0);
-  rc = os_mbuf_pool_init(&sdu_os_mbuf_pool, &sdu_coc_mbuf_mempool, MTU,
-                         COC_BUF_COUNT);
+  rc = os_mbuf_pool_init(&sdu_os_mbuf_pool, &sdu_coc_mbuf_mempool,
+                         COC_MBUF_MEMBLOCK_SIZE, COC_BUF_COUNT);
   assert(rc == 0);
 }
